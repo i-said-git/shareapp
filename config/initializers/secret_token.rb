eb53439862ebd35850a3126e7d1a5893a5ea44a3
Jeff <jeffreylh2000@gmail.com>
2011-05-08 19:47:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareApp::Application.config.secret_token = 'f4f2374f7e4c5801b99c85a3d910b5ae19dc323270dd525f6f7040ba4e560dd827c8e06c844bdfc56241bd44526d1ea71a236eb5697931e6a37360503615c968'
